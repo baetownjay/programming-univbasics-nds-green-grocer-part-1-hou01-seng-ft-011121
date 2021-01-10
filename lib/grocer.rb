d9a8d2625070ec9a9eb9a54de972985b80de42be
Jason Herrera <baytownjason@yahoo.com>
2021-01-10 23:54:37 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,6 +3,7 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  binding.pry
   i = 0
   while i < collection.length do
     if collection[i][:item] == name
