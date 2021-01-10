d2103bd9779d171f3a83b6d773d22f8f81f2566e
Jason Herrera <baytownjason@yahoo.com>
2021-01-10 23:57:37 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -9,8 +9,6 @@ def find_item_by_name_in_collection(name, collection)
       return collection[i]
     end
     i += 1
-  end
-end
 
 def consolidate_cart(cart)
   # Consult README for inputs and outputs
