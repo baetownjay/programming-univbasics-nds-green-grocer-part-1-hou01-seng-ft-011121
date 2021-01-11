1883f61135300ea6144de9d8a00a43ef3530b9ee
Jason Herrera <baytownjason@yahoo.com>
2021-01-11 02:14:41 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -6,15 +6,6 @@ def find_item_by_name_in_collection(name, collection)
   i = 0
   while i < collection.length do
     if collection[i][:item] == name
-      
-      
-      
-      
-      
-      
-      
-      
-
       return collection[i]
     end
     i += 1
